d539b6c86ffa4f95f0e8edc845b80f5240026666
lakpa-finju <connectedtofinju1@gmail.com>
2023-03-26 00:29:57 -0400

    changes the dirctory of java script and csss files to jinja default folders to be rendered successfully

--- static/footer.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myFootnav");
-    if (x.className === "footnov") {
-      x.className += " responsive";
-    } else {
-      x.className = "footnov";
-    }
-  }
